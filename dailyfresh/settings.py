dd8dab29667a28147706204525a9ce283070ee3f
hermes <2587196108@qq.com>
2018-12-20 22:19:02 +0800

    df_goods 创建模型TypeInfo GoodsInfo，同步，开启admin站点

--- dailyfresh/settings.py
+++ dailyfresh/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'df_user.apps.DfUserConfig',
+    'df_goods',
+    'tinymce',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -128,3 +130,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
+TINYMCE_DEFAULT_CONFIG = {
+    'theme': 'advanced',
+    'width': 600,
+    'height': 400,
+}
